bb39d919b0e5ccbbf888c784425327dc68deef19
codexEquilizer <sourav.equilizer007@gmail.com>
2025-01-08 12:07:57 +0530

    Implimenting CitiesContext

---
+++ src/Context/CitiesContext.jsx
@@ -0,0 +1,47 @@
+import { createContext, useState, useEffect, useContext } from "react";
+
+const BASE_URL = "http://localhost:8000";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    setIsLoading(true);
+    async function fetchCities() {
+      try {
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        throw new Error("Facing issue with loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext was called outside CitiesProvider");
+  return context;
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export { CitiesProvider, useCities };
